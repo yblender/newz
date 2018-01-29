02fcee1891532741bdab281861c2bfaa2b8a3aee
Yosef <yosefb1@hotmail.com>
2018-01-29 22:28:28 +0000

    updating

--- Newz/src/NewsProvider.ts
+++ Newz/src/NewsProvider.ts
@@ -10,33 +10,54 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class NewsProvider {
-  apiUrl: string = 'https://newsapi.org/v2/everything?q=';
-  apiKey: string = "bf44f18d575b4e97907e99c38d0dcbb3";
+  apiUrl: string = 'https://newsapi.org/v2/';
+  apiKey: string = "&apiKey=bf44f18d575b4e97907e99c38d0dcbb3";
   data: any;
     constructor(public http: Http) {
         console.log('Hello NewsProvider Provider');
   }
-    createRequest(searchTerm: string) {
-      return this.apiUrl + searchTerm + "&apiKey=" + this.apiKey;
-    }
-    searchNews(searchTerm: string) {
+    createRequest(requestUrl: string) {
       
-
       // don't have the data yet
       return new Promise(resolve => {
         // We're using Angular HTTP provider to request the data,
         // then on the response, it'll map the JSON data to a parsed JS object.
         // Next, we process the data and resolve the promise with the new data.
-        this.http.get(this.createRequest(searchTerm))
+        this.http.get(requestUrl)
           .map(res => res.json())
           .subscribe(data => {
             // we've got back the raw data, now generate the core schedule data
             // and save the data for later reference
+            console.log(requestUrl);
             console.log(data);
-            this.data = data.articles;
+            //if (data.totalResults = 0) {
+            //  this.data = [];
+            //}
+            //else {
+              this.data = data.articles;
+           // }            
+            //console.log(data);
             resolve(this.data);
           });
       });
     }
+    searchNews(searchTerm: string) {      
+      var requestUrl = this.apiUrl + "everything?q=" + searchTerm + this.apiKey;
+      return this.createRequest(requestUrl);      
+    }
+    getHeadlines(country: string, page: number) {
+      var requestUrl = this.apiUrl + "top-headlines?country=" + country + "&page=" + page + this.apiKey;
+      return this.createRequest(requestUrl);
+    } 
+    countryOptions = [{
+      name: "Australia",
+      value: "au"
+    },{
+      name: "Russia",
+      value: "ru"
+    }, {
+      name: "United Kingdom",
+      value: "gb"
+    }];
 
 }
--- Newz/src/pages/home/home.ts
+++ Newz/src/pages/home/home.ts
@@ -11,15 +11,27 @@ export class HomePage {
  
   articles: any;
   searchTerm: string;
+  page: number;
   urlString: string;
+  shownGroup = null;
+  countries: any;
+  selectedCountry: any
+  selectedValue: any;
+  
   constructor(public navCtrl: NavController, private news: NewsProvider) {
-    this.getNews();
+    this.page = 1;
+    
+    this.countries = news.countryOptions;
+    this.selectedCountry = this.getCountry("gb");
+    this.selectedValue = this.selectedCountry.value;
+    this.getHeadlines();
+    console.log(this.countries)
   }
+
   getNews() {
     if (this.searchTerm == undefined || this.searchTerm == "") {
       return;
-    }
-    this.urlString = this.news.createRequest(this.searchTerm);
+    }    
     this.news.searchNews(this.searchTerm)
       .then(data => {
         this.articles = data;
@@ -28,8 +40,59 @@ export class HomePage {
         console.log(error);
       });    
   }
-  onLink(url: string) {
-    window.open(url);
-    
-  }  
+  getHeadlines() {
+    this.page = 1;
+    console.log(this.selectedCountry);
+    this.news.getHeadlines(this.selectedCountry.value, this.page)
+      .then(data => {
+        this.articles = data;
+      })
+      .catch(error => {
+        console.log(error);
+      }); 
+  }
+  changeCountry(selectedCountry: any) {
+    console.log(selectedCountry);
+    this.selectedCountry = this.getCountry(selectedCountry);
+    this.selectedValue = this.selectedCountry.value;
+    this.getHeadlines();
+  }
+  getMoreHeadlines() {
+    this.page++;
+    this.news.getHeadlines(this.selectedCountry, this.page)
+      .then(data => {
+        console.log(data);
+        if (data != []) {
+          
+          this.articles.push(data);
+        }
+        
+        
+      })
+      .catch(error => {
+        console.log(error);
+      }); 
+  }
+  toggleGroup(group) {
+    if (this.isGroupShown(group)) {
+      this.shownGroup = null;
+    } else {
+      this.shownGroup = group;
+    }
+  };
+  isGroupShown(group) {
+    return this.shownGroup === group;
+  };
+  getCountry(isoCode: any) {
+    var foundCountry;
+    this.countries.forEach(function (country) {
+      console.log(country);
+      if (isoCode == country.value) {
+        foundCountry = country;
+      }
+
+    });
+    console.log("getCountry:" + foundCountry.name + " " + foundCountry.value);
+    return foundCountry;
+  }
 }
